45658b8bbae838792272af582aac14e3f055fcd5
Maarten Faddegon <mf357@kent.ac.uk>
2014-07-17 20:55:12 +0100

    split off more generic code into Runtime in preperation of making two similar Semantics

---
+++ src/Runtime.hs
@@ -0,0 +1,103 @@
+module Runtime where
+
+import Control.Monad.State
+import Data.Graph.Libgraph
+
+
+--------------------------------------------------------------------------------
+-- Stack handling: push and call.
+
+type Label = String
+type Stack = [Label]
+
+push :: Label -> Stack -> Stack
+push l s
+  | l `elem` s = dropWhile (/= l) s
+  | otherwise  = l : s
+
+call :: Stack -> Stack -> Stack
+call sApp sLam = sApp ++ sLam'
+  where (sPre,sApp',sLam') = commonPrefix sApp sLam
+
+commonPrefix :: Stack -> Stack -> (Stack, Stack, Stack)
+commonPrefix sApp sLam
+  = let (sPre,sApp',sLam') = span2 (==) (reverse sApp) (reverse sLam)
+    in (reverse sPre, reverse sApp', reverse sLam') 
+
+span2 :: (a -> a -> Bool) -> [a] -> [a] -> ([a], [a], [a])
+span2 f = s f []
+  where s _ pre [] ys = (pre,[],ys)
+        s _ pre xs [] = (pre,xs,[])
+        s f pre xs@(x:xs') ys@(y:ys') 
+          | f x y     = s f (x:pre) xs' ys'
+          | otherwise = (pre,xs,ys)
+
+--------------------------------------------------------------------------------
+-- The state.
+
+type ReduceFun expr = Stack -> Trace -> expr 
+                      -> State (Context expr) (Stack,Trace,ExprExc expr)
+
+data ExprExc expr = Exception String
+                  | Expression expr
+                  deriving Eq
+
+data Context expr = Context { theHeap       :: !(Heap expr)
+                            , theTrace      :: !Trace
+                            , steps         :: !Int
+                            }
+
+estate0 :: Context expr
+estate0 = Context heap0 trace0 0
+
+evalE' :: ReduceFun expr -> expr -> (Stack,Trace,ExprExc expr)
+evalE' eval e = evalState f estate0
+  where f = evalUpto eval [] [] e
+
+evalE :: ReduceFun expr -> expr -> Trace
+evalE eval e = let (_,t,_) = evalE' eval e in t
+
+evalUpto :: ReduceFun expr ->  Stack -> Trace -> expr 
+         -> State (Context expr) (Stack,Trace,ExprExc expr)
+evalUpto eval stk trc expr = do n <- gets steps
+                                modify $ \s -> s {steps = n+1}
+                                if n > 500
+                                  then return (stk,trc,Exception "Too many steps!")
+                                  else eval stk trc expr
+
+--------------------------------------------------------------------------------
+-- Manipulating the heap.
+
+type Name = String
+type Heap expr = [(Name,(Stack,expr))]
+
+heap0 :: Heap expr
+heap0 = []
+
+insertHeap :: Name -> (Stack,expr) -> State (Context expr) ()
+insertHeap x e = modify $ \s -> s{theHeap = (x,e) : (theHeap s)}
+
+deleteHeap :: Name -> State (Context expr) ()
+deleteHeap x = modify $ \s -> s{theHeap = filter ((/= x) . fst) (theHeap s)}
+
+lookupHeap :: Name -> State (Context expr) (Stack,ExprExc expr)
+lookupHeap x = do 
+  me <- fmap (lookup x . theHeap) get
+  case me of
+    Nothing      -> return ([], Exception "Lookup failed")
+    Just (stk,e) -> return (stk,Expression e)
+
+--------------------------------------------------------------------------------
+-- Tracing.
+--
+-- A recorded value is Right or Wrong.
+
+data Value  = Right | Wrong       deriving (Show,Eq,Ord)
+type Record = (Label,Stack,Value)
+type Trace  = [Record]
+
+trace0 :: Trace
+trace0 = []
+
+trace :: Record -> Trace -> Trace
+trace = (:)
--- src/Semantics.hs
+++ src/Semantics.hs
@@ -2,41 +2,11 @@ import Control.Monad.State
 import Prelude hiding (Right)
 import Test.QuickCheck
 import Data.Graph.Libgraph
-
+import Runtime
 import qualified Debug.Trace as Debug
 
 --------------------------------------------------------------------------------
--- Stack handling: push and call
-
-type Label = String
-type Stack = [Label]
-
-push :: Label -> Stack -> Stack
-push l s
-  | l `elem` s = dropWhile (/= l) s
-  | otherwise  = l : s
-
-call :: Stack -> Stack -> Stack
-call sApp sLam = sApp ++ sLam'
-  where (sPre,sApp',sLam') = commonPrefix sApp sLam
-
-commonPrefix :: Stack -> Stack -> (Stack, Stack, Stack)
-commonPrefix sApp sLam
-  = let (sPre,sApp',sLam') = span2 (==) (reverse sApp) (reverse sLam)
-    in (reverse sPre, reverse sApp', reverse sLam') 
-
-span2 :: (a -> a -> Bool) -> [a] -> [a] -> ([a], [a], [a])
-span2 f = s f []
-  where s _ pre [] ys = (pre,[],ys)
-        s _ pre xs [] = (pre,xs,[])
-        s f pre xs@(x:xs') ys@(y:ys') 
-          | f x y     = s f (x:pre) xs' ys'
-          | otherwise = (pre,xs,ys)
-
---------------------------------------------------------------------------------
 -- Expressions.
-
-type Name = String
 
 data Expr = Const
           | Lambda Name Expr
@@ -46,22 +16,24 @@ data Expr = Const
           | ACCCorrect Label Expr
           | ACCFaulty  Label Expr
           | Observed   Label Stack Expr
-          | Exception  String
-
           deriving (Show,Eq)
 
 --------------------------------------------------------------------------------
 -- The reduction rules.
 
-eval :: Stack -> Trace -> Expr -> E (Stack,Trace,Expr)
+eval :: Stack -> Trace -> Expr -> State (Context Expr) (Stack,Trace,ExprExc Expr)
 
-eval stk trc Const = return (stk,trc,Const)
+eval stk trc Const = 
+  return (stk,trc,Expression Const)
 
-eval stk trc (Lambda x e) = return (stk,trc,Lambda x e)
+eval stk trc (Lambda x e) = 
+  return (stk,trc,Expression $ Lambda x e)
 
-eval stk trc (ACCCorrect l e) = evalUpto (push l stk) trc (Observed l stk e)
+eval stk trc (ACCCorrect l e) =
+  evalUpto eval (push l stk) trc (Observed l stk e)
 
-eval stk trc (ACCFaulty l e)  = evalUpto (push l stk) (trace (l,stk,Wrong) trc) e
+eval stk trc (ACCFaulty l e)  =
+  evalUpto eval (push l stk) (trace (l,stk,Wrong) trc) e
 
 eval stk trc (Let (x,e1) e2) = do
   insertHeap x (stk,e2)
@@ -71,77 +43,38 @@ eval stk trc (Let (x,e1) e2) = do
 -- expressions. And we break out of endless loops by returning a Const when we
 -- detect such a loop.
 eval stk trc orig@(Apply f x) = do
-  (stk_lam, trc_lam, e) <- evalUpto stk trc f
+  (stk_lam, trc_lam, e) <- evalUpto eval stk trc f
   case e of 
-    Lambda y e -> evalUpto stk_lam trc_lam (subst y x e)
-    _          -> return (stk_lam,trc_lam,Exception "Apply non-Lambda?")
+    Expression (Lambda y e) -> evalUpto eval stk_lam trc_lam (subst y x e)
+    Exception msg           -> return (stk_lam,trc_lam,Exception msg)
+    _                       -> return (stk_lam,trc_lam,Exception "Apply non-Lambda?")
 
 eval stk trc (Var x) = do
   r <- lookupHeap x
   case r of
-    (stk',Const)  -> return (stk',trc,Const)
-    (stk',Lambda y e) -> return (call stk stk',trc,Lambda y e)
-    (stk',e) -> do
+    (stk',Exception msg)           -> return (stk',trc,Exception msg)
+    (stk',Expression Const)        -> return (stk',trc,Expression Const)
+    (stk',Expression (Lambda y e)) -> return (call stk stk',trc,Expression (Lambda y e))
+    (stk',Expression e) -> do
       deleteHeap x
-      (stkv,trcv,v) <- evalUpto stk' trc e
-      insertHeap x (stkv,v)
-      evalUpto stk trcv (Var x) -- Notice how we retain the trace but swap back the stack
+      (stkv,trcv,v') <- evalUpto eval stk' trc e
+      case v' of
+        Exception msg -> return (stkv,trcv,Exception msg)
+        Expression v  -> do
+          insertHeap x (stkv,v)
+          evalUpto eval stk trcv (Var x) -- Notice how we retain the trace but swap back the stack
 
 -- MF TODO: Ik denk dat alle gevallen hier behandeld moeten worden ipv de _ op het eind?
 eval stk trc (Observed l s e) = do
-  case e of Const              -> return (stk,trace (l,s,Right) trc, Const)
-            (ACCFaulty l' e')  -> evalUpto stk (trace (l,s,Wrong) trc) (ACCFaulty l' e')
-            (ACCCorrect l' e') -> evalUpto stk trc (ACCCorrect l' (Observed l s e'))
-            (Let (x,e1) e2)    -> evalUpto stk trc (Let (x,e1) (Observed l s e2))
+  case e of Const              -> return (stk,trace (l,s,Right) trc,Expression Const)
+            (ACCFaulty l' e')  -> evalUpto eval stk (trace (l,s,Wrong) trc) (ACCFaulty l' e')
+            (ACCCorrect l' e') -> evalUpto eval stk trc (ACCCorrect l' (Observed l s e'))
+            (Let (x,e1) e2)    -> evalUpto eval stk trc (Let (x,e1) (Observed l s e2))
             _ -> do
-              (stk',trc',e') <- evalUpto stk trc e
-              evalUpto stk' trc' (Observed l s e')
-
-
-eval stk trc (Exception s) = return (stk,trc,Exception s)
-
-
-evalUpto :: Stack -> Trace -> Expr -> E (Stack,Trace,Expr)
-evalUpto stk trc expr = do n <- gets steps
-                           -- let n = Debug.trace (  "step" ++ show n' ++ ": " ++ show expr ++ "\n"
-                           --                     ++ "  with trace " ++ show trc
-                           --                     ) 
-                           --                     n'
-                           modify $ \s -> s {steps = n+1}
-                           if n > 500
-                             then return (stk,trc,Exception "Too many steps!")
-                             else eval stk trc expr
-
---------------------------------------------------------------------------------
--- The state.
-
-data EState = EState { theHeap      :: ![(Name,(Stack,Expr))]
-                     , steps        :: !Int
-                     }
-
-type E a = State EState a
-
-evalE' :: Expr -> (Stack,Trace,Expr)
-evalE' e = evalState (evalUpto [] [] e) (EState [] 0)
-
-evalE :: Expr -> Trace
-evalE e = let (_,t,_) = evalE' e in t
-
---------------------------------------------------------------------------------
--- Manipulating the heap
-
-insertHeap :: Name -> (Stack,Expr) -> E ()
-insertHeap x e = modify $ \s -> s{theHeap = (x,e) : (theHeap s)}
-
-deleteHeap :: Name -> E ()
-deleteHeap x = modify $ \s -> s{theHeap = filter ((/= x) . fst) (theHeap s)}
-
-lookupHeap :: Name -> E (Stack,Expr)
-lookupHeap x = do 
-  me <- fmap (lookup x . theHeap) get
-  case me of
-    Nothing -> return ([], Const) -- Keep going with a Const if we find nothing.
-    Just e  -> return e
+              (stk',trc',e') <- evalUpto eval stk trc e
+              case e' of
+                Exception msg  -> return (stk',trc',Exception msg)
+                Expression e'' -> evalUpto eval stk' trc' (Observed l s e'')
 
 --------------------------------------------------------------------------------
 -- Substituting variable names.
@@ -159,22 +92,10 @@ sub :: Name -> Name -> Name -> Name
 sub n m n' = if n == n' then n' else m
 
 --------------------------------------------------------------------------------
--- Tracing.
---
--- A recorded value is Right or Wrong.
-
-data Value  = Right | Wrong       deriving (Show,Eq,Ord)
-type Record = (Label,Stack,Value)
-type Trace  = [Record]
-
-trace :: Record -> Trace -> Trace
-trace = (:)
-
---------------------------------------------------------------------------------
 -- Algorithmic debugging from a trace.
 
 faultyNodes :: Expr -> [Label]
-faultyNodes = getLabels . (findFaulty wrongCC mergeCC) . mkGraph . evalE 
+faultyNodes = getLabels . (findFaulty wrongCC mergeCC) . mkGraph . (evalE eval)
 
 wrongCC :: Vertex -> Bool
 wrongCC (Vertex rs) = foldl (\w r -> case r of (_,_,Wrong) -> True; _ -> w) False rs
@@ -198,23 +119,6 @@ arcsFrom src = (map (Arc src)) . (filter (src `couldDependOn`))
 
 couldDependOn :: Record -> Record -> Bool
 couldDependOn (l,s,_) (_,t,_) = push l s == t
-
--- algoDebug :: Graph Vertex -> [Label]
--- algoDebug g@(Graph _ vs as) = foldl accLabels [] (filter faulty vs)
---         where accLabels acc v = acc ++ getLabels v
---               faulty :: Vertex -> Bool
---               faulty v = isWrong v && (null $ (filter isWrong) (succs g v))
-
-
--- getLabels :: Vertex -> [Label]
--- getLabels (Vertex rs) = map (\(l,_,_) -> l) rs
-
-
---------------------------------------------------------------------------------
--- Removing cycles from a graph.
-
--- rmCycles :: Graph Vertex -> Graph Vertex
--- rmCycles g = dagify merge g
 
 mergeCC :: [Vertex] -> Vertex
 mergeCC ws = foldl merge' (Vertex []) ws
@@ -254,7 +158,7 @@ instance Arbitrary Expr where
   arbitrary = sized gen_expr
 
 propValidExpr :: Expr -> Bool
-propValidExpr e = let (_,_,e') = evalE' e in e' == Const
+propValidExpr e = let (_,_,e') = (evalE' eval) e in e' == Expression Const
 
 propExact :: Expr -> Bool
 propExact e = faultyNodes e == faultyExprs e
@@ -269,7 +173,7 @@ propFoundFaulty :: Expr -> Bool
 propFoundFaulty e = faultyNodes e /= []
 
 propIsWrong :: Expr -> Bool
-propIsWrong e = case lookupT "toplevel" (evalE $ Observed "toplevel" [] e) of
+propIsWrong e = case lookupT "toplevel" (evalE eval $ Observed "toplevel" [] e) of
   (Just Wrong) -> True
   _            -> False
 
@@ -282,7 +186,7 @@ propFaultyIfWrong e = propIsWrong e ==> propFoundFaulty e
 main = quickCheckWith args (\e -> propValidExpr e ==> propFaultyIfWrong e)
   where args = Args { replay          = Nothing
                     , maxSuccess      = 100000  -- number of tests
-                    , maxDiscardRatio = 10
+                    , maxDiscardRatio = 100
                     , maxSize         = 1000    -- max subexpressions
                     , chatty          = True
                     }
@@ -308,33 +212,33 @@ expr4 = Let ("n", Const) (Var "n")
 test1 = propExact expr1
 
 -- Doesn't terminate:
-test2  = evalE $ Apply (Lambda "y" (Apply (                 (Lambda "z" ((Apply (Var "y") "z")))) "z")) "z"
+test2  = evalE eval $ Apply (Lambda "y" (Apply (                 (Lambda "z" ((Apply (Var "y") "z")))) "z")) "z"
 
-test2b = evalE $ Apply (Lambda "y" (Apply (Let ("x",Const) (Lambda "z" ((Apply (Var "y") "z")))) "z")) "z"
+test2b = evalE eval $ Apply (Lambda "y" (Apply (Let ("x",Const) (Lambda "z" ((Apply (Var "y") "z")))) "z")) "z"
 
 
-test2c = evalE $ Apply ((Apply (Let ("z",Apply Const "x") (Lambda "z" (Apply ((Apply ((Apply (Var "x") "x")) "y")) "y"))) "z")) "z"
+test2c = evalE eval $ Apply ((Apply (Let ("z",Apply Const "x") (Lambda "z" (Apply ((Apply ((Apply (Var "x") "x")) "y")) "y"))) "z")) "z"
 
-test2d = evalE $ Apply (ACCCorrect "E" (Apply (Let ("z",Apply Const "x") (Lambda "z" (Apply (ACCCorrect "O" (Apply (ACCCorrect "D" (Apply (Var "x") "x")) "y")) "y"))) "z")) "z"
+test2d = evalE eval $ Apply (ACCCorrect "E" (Apply (Let ("z",Apply Const "x") (Lambda "z" (Apply (ACCCorrect "O" (Apply (ACCCorrect "D" (Apply (Var "x") "x")) "y")) "y"))) "z")) "z"
 
 -- lookup failed?
 
 test3a = faultyNodes $ ACCFaulty "A" (ACCCorrect "B" Const)
-test3b = (display showGraph) . mkGraph . evalE $ ACCFaulty "A" (ACCCorrect "B" Const)
--- test3c = (display showGraph) . rmCycles . mkGraph . evalE $ ACCFaulty "L" (ACCFaulty "L" (Var "z"))
+test3b = (display showGraph) . mkGraph . evalE eval $ ACCFaulty "A" (ACCCorrect "B" Const)
+-- test3c = (display showGraph) . rmCycles . mkGraph . evalE eval $ ACCFaulty "L" (ACCFaulty "L" (Var "z"))
 
 e4 = ACCFaulty "O" (ACCCorrect "C" (Let ("y",Const) (ACCCorrect "R" (ACCCorrect "F" (Var "y")))))
 
-test4a = (display showGraph) . mkGraph . evalE $ e4
+test4a = (display showGraph) . mkGraph . evalE eval $ e4
 
-test4b = evalE e4
+test4b = evalE eval e4
 
 e5 = ACCFaulty "OUTER" (ACCCorrect "INNER" (Let ("x",Const) (Var "x")))
 
 e6 = ACCFaulty "A" (ACCCorrect "B" (ACCCorrect "C" (ACCFaulty "D" (Var "x"))))
 
-test = (display showGraph) . (dagify mergeCC) . mkGraph . evalE
-test' = (display showGraph) . mkGraph . evalE
+test = (display showGraph) . (dagify mergeCC) . mkGraph . evalE eval
+test' = (display showGraph) . mkGraph . evalE eval
 
 e7 = ACCFaulty "A" (ACCCorrect "B" (Apply (ACCFaulty "C" (Apply (Let ("z",Apply (Apply (Var "y") "x") "x") (Lambda "z" (ACCFaulty "D" (ACCFaulty "E" (Var "x"))))) "z")) "y"))
 
