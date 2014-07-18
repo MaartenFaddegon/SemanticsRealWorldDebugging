b26fdec940339bbb68261a9e763eb8387d4256e8
Maarten Faddegon <mf357@kent.ac.uk>
2014-07-18 12:20:47 +0100

    TraceSemantics

---
+++ src/TraceSemantics.hs
@@ -0,0 +1,116 @@
+module IntendedSemantics where
+
+import Control.Monad.State(State)
+import Prelude hiding (Right)
+import Context
+
+
+--------------------------------------------------------------------------------
+-- Tracing.
+
+type Value  = String
+type Record = (Label,Stack,Value)
+
+trace :: Record -> Trace Record -> Trace Record
+trace = (:)
+
+--------------------------------------------------------------------------------
+-- Expressions.
+
+data Expr = Const    Int
+          | Lambda   Name Expr
+          | Apply    Expr Name
+          | Var      Name
+          | Let      (Name,Expr) Expr
+          | ACC      Label Expr
+          | Observed Label Stack Expr
+          deriving (Show,Eq)
+
+--------------------------------------------------------------------------------
+-- The reduction rules.
+
+reduce :: Stack -> Trace Record -> Expr -> State (Context Expr) (Stack,Trace Record,ExprExc Expr)
+
+reduce stk trc (Const i) = 
+  return (stk,trc,Expression (Const i))
+
+reduce stk trc (Lambda x e) = 
+  return (stk,trc,Expression $ Lambda x e)
+
+reduce stk trc (ACC l e) =
+  evalUpto reduce (push l stk) trc (Observed l stk e)
+
+reduce stk trc (Let (x,e1) e2) = do
+  insertHeap x (stk,e1)
+  reduce stk trc e2
+
+reduce stk trc orig@(Apply f x) = do
+  (stk_lam, trc_lam, e) <- evalUpto reduce stk trc f
+  case e of 
+    Expression (Lambda y e) -> evalUpto reduce stk_lam trc_lam (subst y x e)
+    Exception msg           -> return (stk_lam,trc_lam,Exception msg)
+    _                       -> return (stk_lam,trc_lam,Exception "Apply non-Lambda?")
+
+reduce stk trc (Var x) = do
+  r <- lookupHeap x
+  case r of
+    (stk',Exception msg)           -> return (stk',trc,Exception msg)
+    (stk',Expression (Const i))    -> return (stk',trc,Expression (Const i))
+    (stk',Expression (Lambda y e)) -> return (call stk stk',trc,Expression (Lambda y e))
+    (stk',Expression e) -> do
+      deleteHeap x
+      (stkv,trcv,v') <- evalUpto reduce stk' trc e
+      case v' of
+        Exception msg -> return (stkv,trcv,Exception msg)
+        Expression v  -> do
+          insertHeap x (stkv,v)
+          evalUpto reduce stk trcv (Var x) -- Notice how we retain the trace but swap back the stack
+
+-- MF TODO: Ik denk dat alle gevallen hier behandeld moeten worden ipv de _ op het eind?
+reduce stk trc (Observed l s e) = do
+  (stk',trc',e') <- evalUpto reduce stk trc e
+  case e' of
+    Exception msg           -> return (stk',trc',Exception msg)
+    Expression (Const i)    -> return (stk',trace (l,s,show i) trc',Expression (Const i))
+    Expression (Lambda x e) -> do
+      let x' = "_" ++ x; x'' = "__" ++ x
+          lRes = l ++ "-result"; lArg = l ++ "-argument"
+          innerLam = Lambda x (Observed lRes s e)
+          body     = Let (x',Observed lArg stk (Var x'')) 
+                         (Apply innerLam x')
+      evalUpto reduce stk' trc' (Lambda x'' body)
+
+--------------------------------------------------------------------------------
+-- Substituting variable names.
+
+subst :: Name -> Name -> Expr -> Expr
+subst n m (Const i)         = Const i
+subst n m (Lambda n' e)     = Lambda (sub n m n') (subst n m e)
+subst n m (Apply e n')      = Apply (subst n m e) (sub n m n')
+subst n m (Var n')          = Var (sub n m n')
+subst n m (Let (n',e1) e2)  = Let ((sub n m n'),(subst n m e1)) (subst n m e2)
+subst n m (ACC l e)         = ACC l (subst n m e)
+subst n m (Observed l s e)  = Observed l s (subst n m e)
+
+sub :: Name -> Name -> Name -> Name
+sub n m n' = if n == n' then m else n'
+
+
+--------------------------------------------------------------------------------
+-- Examples.
+
+eval   = evalE reduce
+eval' = evalE' reduce
+
+e1 = ACC "A" (Const 42)
+
+e2 = Let ("x", Const 42) (ACC "X" (Var "x"))
+
+e3 = Let ("i", (Const 42)) 
+         (Apply (Lambda "x" (Var "x")) "i")
+
+e4 = Let ("i", (Const 42)) 
+         (Apply (ACC "lam" (Lambda "x" (Var "x"))) "i")
+
+e5 = Let ("i", (Const 42)) 
+         (Apply (ACC "lam" (Lambda "x" (Const 1))) "i")
