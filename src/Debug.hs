2f12473a4ea63ac75dd1e3aee0fb42ab36a7a8c5
Maarten Faddegon <mf357@kent.ac.uk>
2014-07-26 15:32:23 +0100

    couldDependOn2: relations based on call behavious of the css

--- src/Debug.hs
+++ src/Debug.hs
@@ -14,7 +14,27 @@ mkGraph' trace = Graph (head roots)
   where roots = filter (\rec -> recordStack rec == []) trace
 
 arcsFrom :: (Record value) -> Trace value -> [Arc (Record value)]
-arcsFrom src = (map (Arc src)) . (filter (src `couldDependOn`))
+arcsFrom src trc = (map (Arc src)) . (filter couldDependOn) $ trc
+  where couldDependOns = couldDependOn1 src 
+                         :  map (couldDependOn2 src) trc
+                         ++ map (flip couldDependOn2 src) trc
+        couldDependOn  = yna couldDependOns
 
-couldDependOn :: (Record value) -> (Record value) -> Bool
-couldDependOn p c = push (recordLabel p) (recordStack p) == (recordStack c)
+
+-- The reverse of any from the prelude
+yna :: [a->Bool] -> a -> Bool
+yna ps x = or (map (\p -> p x) ps)
+
+
+
+
+couldDependOn1 :: (Record value) -> (Record value) -> Bool
+couldDependOn1 p c = push (recordLabel p) (recordStack p) == recordStack c
+
+couldDependOn2 :: (Record value) -> (Record value) -> (Record value) -> Bool
+couldDependOn2 p1 p2 c = let s = push (recordLabel p1) (recordStack p1)
+                             t = push (recordLabel p2) (recordStack p2)
+                         in call s t == recordStack c
+
+
+
